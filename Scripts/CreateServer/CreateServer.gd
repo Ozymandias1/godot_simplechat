919aa357764abb04cf99bb6ebdb67b10c79949f6
Ozymandias1 <mrbaek86@gmail.com>
2024-06-13 02:48:34 +0900

    서버 생성
    - 생성 버튼 클릭시 플레이어 정보 얻기

--- Scripts/CreateServer/CreateServer.gd
+++ Scripts/CreateServer/CreateServer.gd
@@ -1,6 +1,14 @@
 # CreateServer.gd 서버 설정 스크립트
 extends Control
 
+@onready var player_setup = $Container/SetupContainer/PlayerSetup
+
+# 생성 버튼 클릭 시그널
+func _on_btn_create_pressed():
+	var player_info: Dictionary = player_setup.get_player_info()
+	print(player_info)
+	
 # 취소 버튼 클릭 시그널
 func _on_btn_cancel_pressed():
 	get_tree().change_scene_to_file("res://Scenes/MainMenu/MainMenu.tscn")
+
--- Scripts/PlayerSetup/PlayerSetup.gd
+++ Scripts/PlayerSetup/PlayerSetup.gd
@@ -25,6 +25,15 @@ func _on_btn_right_pressed():
 		selected_tex = 0
 	player_sprite.texture = player_textures[selected_tex]
 
+# 플레이어 정보 얻기
+func get_player_info():
+	# 플레이어 이름이 입력되지 않았다면 임의의 이름으로 설정한다.
+	var player_name = line_edit_nick_name.text
+	if player_name.is_empty():
+		player_name = "사용자%d" % randi_range(1, 100)
+
+	return { "player_name": player_name, "tex_index": selected_tex }
+
 #func _on_line_edit_nick_name_text_submitted(new_text):
 	#print(new_text)
 	#if new_text.is_empty():
