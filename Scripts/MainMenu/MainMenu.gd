c5ed5ae0ba4f6081420cd06df8915524b8a0e74a
Ozymandias1 <mrbaek86@gmail.com>
2024-06-13 02:29:17 +0900

    서버 생성 화면

--- Scripts/MainMenu/MainMenu.gd
+++ Scripts/MainMenu/MainMenu.gd
@@ -1,5 +1,8 @@
 extends Control
 
+# 서버 생성 버튼 클릭 시그널
+func _on_btn_create_server_pressed():
+	get_tree().change_scene_to_file("res://Scenes/CreateServer/CreateServer.tscn")
 
 # 메인메뉴 Quit 버튼 클릭 시그널
 func _on_btn_quit_pressed():
